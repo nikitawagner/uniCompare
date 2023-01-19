2ea0a4a6344ecb1af02f58b7e5565226db114d9e
Nikita Wagner <nikita.leon.wagner@gmail.com>
2023-01-19 15:39:32 +0100

    feature: add buttons for save and show uni subject

--- uni-compare/src/App.jsx
+++ uni-compare/src/App.jsx
@@ -6,56 +6,17 @@ import ostfalia from "./ostfalia.json";
 import paris from "./paris.json";
 
 function App() {
-  // let datenbanken = {
-  //   id: 1,
-  //   name: "Datenbanken",
-  //   cp: 5,
-  //   color: null,
-  //   description: "ER-Diagramme, SQL",
-  //   requirements: "/",
-  //   marked: false,
-  // };
-
-  // let soe = {
-  //   id: 2,
-  //   name: "Software Engineering",
-  //   cp: 5,
-  //   color: null,
-  //   requirements: "/",
-  //   description: "Design Patterns, GIT, SCRUM",
-  //   marked: false,
-  // };
-
-  // let gdp = {
-  //   id: 3,
-  //   name: "Grundlagen der Programmierung",
-  //   cp: 10,
-  //   color: null,
-  //   requirements: "/",
-  //   description: "Basic Java Knowledge (Loops, if, Objects...)",
-  //   marked: false,
-  // };
-
-  // let coma = {
-  //   id: 4,
-  //   name: "Computermathematik",
-  //   cp: 5,
-  //   color: null,
-  //   requirements: "/",
-  //   description: "Mathe halt",
-  //   marked: false,
-  // };
-
-  // const [subjectArray, setSubjectArray] = useState([
-  //   datenbanken,
-  //   soe,
-  //   gdp,
-  //   coma,
-  // ]);
+  const [ostfaliaJson, setOstfaliaJson] = useState(ostfalia);
+  const [parisJson, setParisJson] = useState(paris);
 
   return (
     <div className="App">
-      <CardList ostfalia={ostfalia} paris={paris} />
+      <CardList
+        ostfalia={ostfaliaJson}
+        paris={parisJson}
+        setOstfalia={(updatedJson) => setOstfaliaJson(updatedJson)}
+        setParis={(updatedJson) => setParisJson(updatedJson)}
+      />
     </div>
   );
 }
--- uni-compare/src/components/CardList.jsx
+++ uni-compare/src/components/CardList.jsx
@@ -1,43 +1,68 @@
+import { Button, IconButton } from "@mui/material";
 import { useState } from "react";
 import Card from "./Card";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export default function CardList({ ostfalia, paris }) {
   const [subjectArray, setSubjectArray] = useState(ostfalia.subjects);
   const [subjectArrayParis, setSubjectArrayParis] = useState(paris.subjects);
 
   return (
-    <div className="cardLists">
-      <div className="ostfaliaList">
-        {subjectArray.map((subject, key) => {
-          return (
-            <Card
-              subject={subject}
-              key={key}
-              subjectArray={subjectArray}
-              setSubjectArray={(updatedSubject) =>
-                setSubjectArray(updatedSubject)
-              }
-              colorUni={ostfalia.color}
-            />
-          );
-        })}
+    <>
+      <div>
+        <Button
+          variant="contained"
+          startIcon={<KeyboardArrowUpIcon />}
+          style={{ margin: "10px" }}
+        >
+          Show Ostfalia
+        </Button>
+        <Button variant="contained" color="success" style={{ margin: "10px" }}>
+          Save
+        </Button>
+        <Button
+          variant="contained"
+          startIcon={<KeyboardArrowUpIcon />}
+          style={{ margin: "10px" }}
+        >
+          Show Paris
+        </Button>
       </div>
 
-      <div className="parisList">
-        {subjectArrayParis.map((subject, key) => {
-          return (
-            <Card
-              subject={subject}
-              key={key}
-              subjectArray={subjectArrayParis}
-              setSubjectArray={(updatedSubject) =>
-                setSubjectArrayParis(updatedSubject)
-              }
-              colorUni={paris.color}
-            />
-          );
-        })}
+      <div className="cardLists">
+        <div className="ostfaliaList">
+          {subjectArray.map((subject, key) => {
+            return (
+              <Card
+                subject={subject}
+                key={key}
+                subjectArray={subjectArray}
+                setSubjectArray={(updatedSubject) =>
+                  setSubjectArray(updatedSubject)
+                }
+                colorUni={ostfalia.color}
+              />
+            );
+          })}
+        </div>
+
+        <div className="parisList">
+          {subjectArrayParis.map((subject, key) => {
+            return (
+              <Card
+                subject={subject}
+                key={key}
+                subjectArray={subjectArrayParis}
+                setSubjectArray={(updatedSubject) =>
+                  setSubjectArrayParis(updatedSubject)
+                }
+                colorUni={paris.color}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
